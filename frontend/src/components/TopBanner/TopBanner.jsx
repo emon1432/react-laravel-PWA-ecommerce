09693943a13644e4f067c0ed8dc59e9568616ccc
emon1432 <ovirajemon143@gmail.com>
2023-09-16 18:02:14 +0600

    top banner complete

---
+++ frontend/src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,28 @@
+import React, { Component, Fragment } from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import "../../assets/css/bootstrap.min.css";
+import "../../assets/css/custom.css";
+
+export class TopBanner extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container fluid={true} className="topFixedBanner p-0">
+          <div className="topBannerOverlay">
+            <Container className="topContent">
+              <Row>
+                <Col className="text-center">
+                  <h1 className="topTitle">Easy Learning</h1>
+                  <h4 className="topSubTitle">Learn on your schedule</h4>
+                  <Button variant="primary">Learn More</Button>
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default TopBanner;
