e21960e3f385c219e3a3707b0ab3a9b65b7d5263
emon1432 <ovirajemon143@gmail.com>
2024-09-04 23:28:01 +0600

    refactor: Update Hello component to use controlled input for name

--- frontend/src/components/Hello.js
+++ frontend/src/components/Hello.js
@@ -2,33 +2,20 @@ import React, { Component } from "react";
 
 class Hello extends Component {
   state = {
-    friends: [
-      { name: "naim", age: 25 },
-      { name: "bappy", age: 26 },
-      { name: "raju", age: 27 },
-      { name: "prince", age: 28 },
-    ],
+    name: "React",
   };
 
-  clickFunction = () => {
+  changeName = (event) => {
     this.setState({
-      friends: [
-        { name: "NAIM", age: 25 },
-        { name: "BAPPY", age: 26 },
-        { name: "RAJU", age: 27 },
-        { name: "PRINCE", age: 28 },
-      ],
+      name: event.target.value,
     });
   };
 
   render() {
     return (
       <>
-        <button onClick={this.clickFunction}>Click</button>
-        <h1>Friend 1: {this.state.friends[0].name}</h1>
-        <h1>Friend 2: {this.state.friends[1].name}</h1>
-        <h1>Friend 3: {this.state.friends[2].name}</h1>
-        <h1>Friend 4: {this.state.friends[3].name}</h1>
+        <input type="text" onChange={this.changeName} value={this.state.name} />
+        <h1>Hello {this.state.name}</h1>
       </>
     );
   }
